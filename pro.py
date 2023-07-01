b669bf0be40e3805b62850edbc49f43c485a016b
Bavneet Kaur <80310878+Bavneet-kaur@users.noreply.github.com>
2023-07-01 11:25:05 +0530

    Updated the forking file

--- pro.py
+++ pro.py
@@ -1,2 +1,6 @@
-print(" Hello world")
-user = input(" Jump here")
+print("Hi! Guys! This is an example of forking a repo")
+num = int(input("Enter Your Fav Number: "))
+if num >= 18:
+  print("number seems pretty awesome", num)
+else:
+  print("oops! this number is so small", num)
